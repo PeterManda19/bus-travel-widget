8641490f75dc87762bf19b8cc15d147b23857029
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-31 14:03:56 +0200

    Factory function and tests

--- busTravelWidget.js
+++ busTravelWidget.js
@@ -1,15 +1,76 @@
 // Application Logic
-// Pseudo code in comments
 
 // Define a function or factory function
 function BusWidget() {
-    // Add necessary properties and methods
-
-    // Define methods for calculating various values
-    // costPerTrip(points, destination)
-    // totalTrips(points, destination, time)
-    // returnTrips(points, destination, time, isReturn)
-    // costPerReturnTrip(points, destination, time, isReturn)
-    // priceBasedOnTime(destination, time)
+    // Define necessary properties
+    const destinations = {
+      "Khayelitsha": 40,
+      "Dunoon": 25,
+      "Mitchells Plain": 35,
+    };
+  
+    // Calculate the cost per trip based on points and destination
+    function costPerTrip(points, destination) {
+      return points * destinations[destination];
+    }
+  
+    // Calculate the total number of trips based on points, destination, and time
+    function totalTrips(points, destination, time) {
+      // Calculate the cost per trip
+      const cost = costPerTrip(points, destination);
+  
+      // Calculate the price based on the time and destination
+      const priceBasedOnTime = calculatePriceBasedOnTime(destination, time);
+  
+      // Calculate the total number of trips needed
+      return Math.ceil(cost / priceBasedOnTime);
+    }
+  
+    // Calculate the number of return trips based on points, destination, time, and return option
+    function returnTrips(points, destination, time, isReturn) {
+      // If return option is not selected, return 0 return trips
+      if (!isReturn) {
+        return 0;
+      }
+  
+      // Calculate the total number of trips
+      const total = totalTrips(points, destination, time);
+  
+      // Calculate the number of return trips (half of total trips)
+      return Math.ceil(total / 2);
+    }
+  
+    // Calculate the cost for return trips based on points, destination, time, and return option
+    function costPerReturnTrip(points, destination, time, isReturn) {
+      // Calculate the number of return trips
+      const returnTripsCount = returnTrips(points, destination, time, isReturn);
+  
+      // Calculate the price based on the time and destination
+      const priceBasedOnTime = calculatePriceBasedOnTime(destination, time);
+  
+      // Calculate the cost for return trips
+      return returnTripsCount * priceBasedOnTime * 2;
+    }
+  
+    // Calculate the price based on the time and destination
+    function calculatePriceBasedOnTime(destination, time) {
+      // Get the base price from the destinations object (which is equal to the points)
+      const basePrice = destinations[destination];
+  
+      // Apply a price multiplier if the time is "peak" (1.25 multiplier), otherwise use base price
+      const priceMultiplier = time === "peak" ? 1.25 : 1;
+  
+      // Calculate the final price based on time
+      return basePrice * priceMultiplier;
+    }
+  
+    // Return an object with the public methods
+    return {
+      costPerTrip,
+      totalTrips,
+      returnTrips,
+      costPerReturnTrip,
+      calculatePriceBasedOnTime,
+    };
 }
   
--- test/busTravelWidget.test.js
+++ test/busTravelWidget.test.js
@@ -1,1 +1,96 @@
-// Test the application logic using Mocha JS testing framework
+describe('BusWidget', function () {
+  let widget;
+
+  beforeEach(function () {
+    // Create a new instance of BusWidget before each test
+    widget = BusWidget();
+  });
+
+  describe('costPerTrip', function () {
+    it('should calculate the cost per trip correctly', function () {
+      const points = 10;
+      const destination = 'Khayelitsha';
+      const expectedCost = 10;
+
+      const cost = widget.costPerTrip(points, destination);
+
+      assert.strictEqual(cost, expectedCost);
+    });
+  });
+
+  describe('totalTrips', function () {
+    it('should calculate the total trips correctly', function () {
+      const points = 20;
+      const destination = 'Dunoon';
+      const time = 'offPeak';
+      const expectedTotalTrips = 20;
+
+      const totalTrips = widget.totalTrips(points, destination, time);
+
+      assert.strictEqual(totalTrips, expectedTotalTrips);
+    });
+  });
+
+  describe('returnTrips', function () {
+    it('should calculate the return trips correctly when return option is selected', function () {
+      const points = 30;
+      const destination = 'Mitchells Plain';
+      const time = 'peak';
+      const isReturn = true;
+      const expectedReturnTrips = 2;
+
+      const returnTrips = widget.returnTrips(points, destination, time, isReturn);
+
+      assert.strictEqual(returnTrips, expectedReturnTrips);
+    });
+
+    it('should return 0 return trips when return option is not selected', function () {
+      const points = 30;
+      const destination = 'Mitchells Plain';
+      const time = 'peak';
+      const isReturn = false;
+      const expectedReturnTrips = 0;
+
+      const returnTrips = widget.returnTrips(points, destination, time, isReturn);
+
+      assert.strictEqual(returnTrips, expectedReturnTrips);
+    });
+  });
+
+  describe('costPerReturnTrip', function () {
+    it('should calculate the cost per return trip correctly', function () {
+      const points = 40;
+      const destination = 'Khayelitsha';
+      const time = 'offPeak';
+      const isReturn = true;
+      const expectedCost = 100;
+
+      const cost = widget.costPerReturnTrip(points, destination, time, isReturn);
+
+      assert.strictEqual(cost, expectedCost);
+    });
+  });
+
+  describe('calculatePriceBasedOnTime', function () {
+    it('should calculate the price based on time correctly for peak time', function () {
+      const destination = 'Dunoon';
+      const time = 'peak';
+      const expectedPrice = 25 * 1.25;
+
+      const price = widget.calculatePriceBasedOnTime(destination, time);
+
+      assert.strictEqual(price, expectedPrice);
+    });
+
+    it('should calculate the price based on time correctly for off-peak time', function () {
+      const destination = 'Mitchells Plain';
+      const time = 'offPeak';
+      const expectedPrice = 35;
+
+      const price = widget.calculatePriceBasedOnTime(destination, time);
+
+      assert.strictEqual(price, expectedPrice);
+    });
+  });
+});
+
