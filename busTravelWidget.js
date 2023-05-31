4f24e5d68f46df63f873e5f4ac39bf75180b2837
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-31 10:56:20 +0200

    factory function Pseudo code in comments

---
+++ busTravelWidget.js
@@ -0,0 +1,15 @@
+// Application Logic
+// Pseudo code in comments
+
+// Define a function or factory function
+function BusWidget() {
+    // Add necessary properties and methods
+
+    // Define methods for calculating various values
+    // costPerTrip(points, destination)
+    // totalTrips(points, destination, time)
+    // returnTrips(points, destination, time, isReturn)
+    // costPerReturnTrip(points, destination, time, isReturn)
+    // priceBasedOnTime(destination, time)
+}
+  
