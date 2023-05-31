4c6741cc6753129394cdfc462dcd98c9dcac3550
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-31 10:59:00 +0200

    DOM Pseudo code in comments

--- busTravelWidget.dom.js
+++ busTravelWidget.dom.js
@@ -1,5 +1,13 @@
 // DOM element(s) references
-
+// Get references to HTML elements using document.getElementById() or other methods
 
 // Factory Function instance 
 
+// DOM Interaction
+// Pseudo code in comments
+
+// Add event listener to the Calculate button to trigger the calculation
+// Inside the event listener, retrieve input values and pass them to the application logic methods
+// Update the result container with the calculated values
+
+// Use DOM manipulation techniques to update the result container with the calculated values
